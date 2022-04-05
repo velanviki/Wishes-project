0e645946d19702304e30f810503effd94e6ee2c2
velan <velanvicky01@gmail.com>
2022-04-05 11:54:49 +0530

    link

--- Client/src/api/index.js
+++ Client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({baseURL : 'https://wishes-mern.herokuapp.com'});
+const API = axios.create({baseURL : 'https://wishes-me.herokuapp.com'});
 
 API.interceptors.request.use((req) => {
 if(localStorage.getItem('profile')){
